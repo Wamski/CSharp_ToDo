14a7b0406b8cbdf9cb6e533541be62c157490292
Wamski <waskyjo@gmail.com>
2025-02-10 13:07:26 -0500

    Added the ability for the program to search a folder in the user's documents for projects, also added a button for adding a todo item

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,5 +1,7 @@
 using System;
+using System.IO;
 using Avalonia.Controls;
+using Avalonia.Media;
 
 
 namespace ToDoApp;
@@ -9,14 +11,11 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        
+        string todoDocs = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/ToDoApp";
+        GetProjects(todoDocs);
     }
     
-    private void OnCreateProjectClicked(object sender, Avalonia.Interactivity.RoutedEventArgs e)
-    {
-        var puCreateProject = new CreateProject(this);
-        puCreateProject.Show();
-    }
-
     public void OnProjectClicked(object sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
         Button btn = sender as Button;
@@ -24,6 +23,37 @@ public partial class MainWindow : Window
 
         ProjectNameOverview.Text = projectName + " Overview";
 
-
     }
+    private void OnCreateProjectClicked(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var puCreateProject = new CreateProject(this);
+        puCreateProject.Show();
+    }
+
+    private void GetProjects(string path)
+    {
+        try
+        {
+            string[] files = Directory.GetFiles(path);
+            foreach (string file in files)
+            {
+                string name = Path.GetFileNameWithoutExtension(file);
+                Button btn = new Button
+                {
+                    Width = 160,
+                    Content = name,
+                    Background = Brushes.Transparent
+                };
+            
+                btn.Click += OnProjectClicked;
+                ProjectStack.Children.Add(btn);
+            }
+        }
+        catch (DirectoryNotFoundException)
+        {
+            Console.WriteLine("Directory Not Found");
+        }
+        
+    }
+    
 }
